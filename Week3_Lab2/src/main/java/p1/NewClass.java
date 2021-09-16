27ffb568990db5e5a6d427d3186e0c04af7d063f
jonat <jonat@137.125.129.49>
2021-09-16 14:26:06 -0400

    part 2

--- Week3_Lab2/src/main/java/p1/NewClass.java
+++ Week3_Lab2/src/main/java/p1/NewClass.java
@@ -13,7 +13,27 @@ public class NewClass {
         else{
             System.out.println(testNum + " is prime");
         }
+        
+        int[] numVars = {3,4,8,7,9,10,12,5};
+        System.out.println("The sum of prime numbers in the array is: " + nc.arrSum(numVars));
+        
+        
    }
+    
+    private int arrSum(int[] numVars)
+    {
+        int sum = 0;
+        for(int i = 0; i < numVars.length; i++)
+        {
+            if(isPrime(numVars[i]) == true)
+            {
+                sum += numVars[i];
+            }
+        }
+        
+        return sum;
+    }
+    
     private boolean isPrime(int n){
         for(int i = 2; i < n-1; i++)
         {
