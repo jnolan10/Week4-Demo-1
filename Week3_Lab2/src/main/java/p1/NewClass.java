88b7d4e6904f5b6c74cdd05aead8ca5b406d9a05
jonat <jonat@137.125.129.49>
2021-09-17 21:59:53 -0400

    added the last method and algorithm to the main

--- Week3_Lab2/src/main/java/p1/NewClass.java
+++ Week3_Lab2/src/main/java/p1/NewClass.java
@@ -1,11 +1,17 @@
 package p1;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Scanner;
 public class NewClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        NewClass nc = new NewClass();
         Scanner scan = new Scanner(System.in);
         System.out.println("Please input a number: ");
         int testNum = scan.nextInt();
-        NewClass nc = new NewClass();
         if(nc.isPrime(testNum) == false)
         {
             System.out.println(testNum + " is not prime");
@@ -17,8 +23,28 @@ public class NewClass {
         int[] numVars = {3,4,8,7,9,10,12,5};
         System.out.println("The sum of prime numbers in the array is: " + nc.arrSum(numVars));
         
+       int[] arrNums = nc.getArray("nums.txt.txt");
+       
+       int sum = nc.arrSum(arrNums);
+        System.out.println("The sum of prime numbers in the array is: " + sum);
+         }
+    
+    private int[] getArray(String fileName) throws FileNotFoundException, IOException{
+        Scanner scan = new Scanner(new File(fileName));
+        int lineCount = 0;
+        BufferedReader read = new BufferedReader(new FileReader(fileName));
+        while(read.readLine() != null)
+            lineCount++;
+        int[] arr = new int[lineCount];
+        int i = 0;
+        while(scan.hasNext())
+        {
+            arr[i] = scan.nextInt();
+            i++;
+        }
         
-   }
+        return arr;
+    }
     
     private int arrSum(int[] numVars)
     {
